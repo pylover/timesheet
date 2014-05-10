6816a5fbed2ee3afa4a3f70cf678fdb312d4c46b
vahid <vahid.mardani@gmail.com>
2014-05-10 07:55:35 +0430

    import

--- timesheet/commands/import_.py
+++ timesheet/commands/import_.py
@@ -1,8 +1,12 @@
+from _ast import Import
+
 __author__ = 'vahid'
 
 from timesheet.commands import Command
+from timesheet import config
 import sys
 import csv
+from datetime import datetime
 from timesheet.models import Subject
 
 
@@ -10,8 +14,23 @@ class ImportCommand(Command):
     name = 'import'
     description = 'Import csv file with columns: subject, task, start, end'
 
+    def __init__(self, *args, **kw):
+        super(ImportCommand, self).__init__(*args, **kw)
+        self.subjects = {}
+
     def add_arguments(self):
         self.parser.add_argument('input', nargs='?', help="Input filename. if omitted, the standard input will be used")
+
+    def process_row(self, row):
+        subject_name, task_name, start_time, end_time = row
+        start_time = datetime.strptime(start_time, config.datetime_format)
+        end_time = datetime.strptime(end_time, config.datetime_format)
+
+        if not subject_name in self.subjects:
+            self.subjects[subject_name] = Subject.ensure(subject_name)
+
+
+        print subject_name, task_name, start_time, end_time
 
     def do_job(self):
 
@@ -23,7 +42,7 @@ class ImportCommand(Command):
         try:
             reader = csv.reader(csv_file)
             for row in reader:
-                print ', '.join(row)
+                self.process_row(row)
         finally:
             if csv_file and csv_file != sys.stdin:
                 csv_file.close()
