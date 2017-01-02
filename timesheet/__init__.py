3e7f3d59d575bdf29d06c2eb8626bd8f5ad80c03
Vahid <vahid.mardani@gmail.com>
2017-01-02 21:18:38 +0330

    Adding filter support for datetime in the `report` command.

--- timesheet/__init__.py
+++ timesheet/__init__.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import sys
 from timesheet.configuration import init_config, config
 __author__ = 'vahid'
-__version__ = '0.9.3'
+__version__ = '0.9.4'
 
 
 def entrypoint():
--- timesheet/commands/full_report.py
+++ timesheet/commands/full_report.py
@@ -3,7 +3,7 @@ from timesheet.commands import Command
 from timesheet.models import Subject, Task
 from timesheet.commands.completers import subject_completer
 from prettytable import PrettyTable
-from datetime import timedelta, date
+from datetime import timedelta, date, datetime
 __author__ = 'vahid'
 
 
@@ -16,6 +16,10 @@ class FullReportCommand(Command):
         cls.parser.add_argument('subject', nargs='?', help="Subject to do something about that.")\
             .completer = subject_completer
         cls.parser.add_argument('--today', action='store_true', help="Only prints today's tasks.")
+        cls.parser.add_argument('--from', dest='from_', help='example: 2016.12.21')
+        cls.parser.add_argument('--to', help='example: 2016.12.21')
+        cls.parser.add_argument('--time-format', default='%Y.%m.%d', help="Date time format to parse --from/--to values"
+                                                                          ", default: %Y.%m.%d")
 
     def report_subject(self, subject):
 
@@ -29,6 +33,12 @@ class FullReportCommand(Command):
         q = Task.query.filter(Task.subject == subject)
         if self.args.today:
             q = q.filter(Task.start_time > date.today())
+
+        elif self.args.from_:
+            q = q.filter(Task.start_time >= self.parse_datetime(self.args.from_))
+
+        elif self.args.to:
+            q = q.filter(Task.start_time <= self.parse_datetime(self.args.to))
 
         q = q.order_by(Task.start_time)
 
@@ -45,6 +55,9 @@ class FullReportCommand(Command):
         h = ts / 3600
         m = (ts % 3600) / 60
         print('Total: %.2d:%.2d\n' % (h, m))
+
+    def parse_datetime(self, string_value):
+        return datetime.strptime(string_value, self.args.time_format)
 
     def do_job(self):
         if self.args.subject:
