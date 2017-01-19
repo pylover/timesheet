81ad61464c1413faef69bc18a12fab3659e85be4
pylover <vahid@carrene.com>
2017-01-19 13:30:03 +0330

    Time limit for export command.

--- timesheet/commands/export.py
+++ timesheet/commands/export.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import sys
 import csv
+from datetime import datetime
+
 from timesheet.commands import Command
 from timesheet.models import Task, Subject
 from timesheet.commands.completers import subject_completer
-__author__ = 'vahid'
 
 
 class ExportCommand(Command):
@@ -20,6 +21,13 @@ class ExportCommand(Command):
                                 default=[],
                                 help="Subject to filter the result ")\
             .completer = subject_completer
+        cls.parser.add_argument('--from', dest='from_', metavar="FROM", help='example: 2016-12-21')
+        cls.parser.add_argument('--to', help='example: 2016-12-21')
+        cls.parser.add_argument('--time-format', default='%Y-%m-%d',
+                                help="Date time format to parse --from/--to values, default: %%Y-%%m-%%d")
+
+    def parse_datetime(self, string_value):
+        return datetime.strptime(string_value, self.args.time_format)
 
     def do_job(self):
 
@@ -33,9 +41,15 @@ class ExportCommand(Command):
             if self.args.subject:
                 query = query.filter(Subject.title.in_(self.args.subject))
 
+            elif self.args.from_:
+                query = query.filter(Task.start_time >= self.parse_datetime(self.args.from_))
+
+            elif self.args.to:
+                query = query.filter(Task.start_time <= self.parse_datetime(self.args.to))
+
             writer = csv.writer(csv_file)
             for task in query:
-                writer.writerow([task.subject.title, task.title, task.start_time_string, task.end_time_string])
+                writer.writerow([task.subject.title, task.title, task.start_time_string, task.end_time_string, task.duration_formatted])
         finally:
             if csv_file and csv_file != sys.stdout:
                 csv_file.close()
